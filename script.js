ba61b160b0f313cf6982ace628f48996364128c1
Sawa7719 <sarah.mzyene77@gmail.com>
2026-07-18 12:56:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
-function ouvrirSceau() {
+function ouvrirPalais(){
 
-    document.getElementById("sceau").style.display = "block";
-
-}
-
-
-
-function entrerPalais() {
-
-
-    let code = document.getElementById("code").value;
-
-
-    let erreur = document.getElementById("erreur");
-
-
-
-    if (code.trim().toLowerCase() === "sowji") {
-
-
-        erreur.innerHTML =
-        "✨ Le sceau reconnaît ton cœur...";
-
-
-        setTimeout(() => {
-
-            window.location.href = "jardin.html";
-
-        }, 1000);
-
-
-    } else {
-
-
-        erreur.innerHTML =
-        "Le sceau reste fermé...";
-
-
-    }
-
+    window.location.href = "jardin.html";
 
 }
