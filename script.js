46b74ea19c5ef6f5636c3b1640913b79996d5fb8
Sawa7719 <sarah.mzyene77@gmail.com>
2026-07-18 12:51:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,41 @@
-/* =================================
-   SOWJI
-   Le cœur magique du palais
-   ================================= */
-
-
-/*
-   Ouverture du sceau
-*/
-
 function ouvrirSceau() {
 
-    let sceau =
-    document.getElementById("sceau");
-
-
-    sceau.style.display = "block";
+    document.getElementById("sceau").style.display = "block";
 
 }
 
 
 
-/*
-   Vérification du mot secret
-*/
-
 function entrerPalais() {
 
 
-    let code =
-    document.getElementById("code").value;
+    let code = document.getElementById("code").value;
 
 
-    let erreur =
-    document.getElementById("erreur");
+    let erreur = document.getElementById("erreur");
 
 
 
-    if(code.toLowerCase() === "sowji") {
+    if (code.trim().toLowerCase() === "sowji") {
 
 
         erreur.innerHTML =
         "✨ Le sceau reconnaît ton cœur...";
 
 
-        setTimeout(function(){
+        setTimeout(() => {
 
-            window.location.href =
-            "jardin.html";
+            window.location.href = "jardin.html";
 
-        },1200);
-
+        }, 1000);
 
 
-    }
-
-    else {
+    } else {
 
 
         erreur.innerHTML =
         "Le sceau reste fermé...";
+
 
     }
 
